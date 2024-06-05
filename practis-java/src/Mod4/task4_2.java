f93f4b69fbb3df3b4acf0a0f6d94bd109e4b5c44
Yuriy Perezhogin <wolfviking@yandex.ru>
2024-06-05 19:11:51 +0500

    add mod4/task4_2

---
+++ practis-java/src/Mod4/task4_2.java
@@ -0,0 +1,28 @@
+package Mod4;
+
+public class task4_2 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 11; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 5; i < 16; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 10; i >= 0; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 17; i >= 9; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < 11; i++) {
+            if (i % 2 == 0){
+                System.out.print(i + " ");
+            }
+
+        }
+    }
+}
