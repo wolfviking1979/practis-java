496f8f410a65408d85b7368b72f7416ee8b42085
Yuriy Perezhogin <wolfviking@yandex.ru>
2024-06-06 16:00:16 +0500

    add mod3/task3_3

--- practis-java/src/Mod4/task4_2.java
+++ practis-java/src/Mod4/task4_2.java
@@ -1,28 +1,81 @@
 package Mod4;
 
+import java.util.Scanner;
+
 public class task4_2 {
+
+    private final static int minIncome = 200000;
+    private final static int maxIncome = 900000;
+
+    private static int officeRentCharge = 14000;
+    private static int telephonyCharge = 12000;
+    private static int internetAccessCharge = 7200;
+
+    private static int assistantSalary = 40000;
+    private static int financeManagerSalary = 90000;
+
+    private static double mainTaxPercent = 0.24;
+    private static double managerPercent = 0.15;
+
+    private static double minInvestmentsAmount = 100000;
+
+
     public static void main(String[] args) {
-        for (int i = 0; i < 11; i++) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        for (int i = 5; i < 16; i++) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        for (int i = 10; i >= 0; i--) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        for (int i = 17; i >= 9; i--) {
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        for (int i = 0; i < 11; i++) {
-            if (i % 2 == 0){
-                System.out.print(i + " ");
+        while (true){
+            System.out.println("Введите сумму компании за месяц" +
+                    " (от 200 до 900 тысяч рублей)");
+
+            int income = (new Scanner(System.in)).nextInt();
+
+            // проверка не выходит ли доход за грани min и max.
+            if (!checkIncomeRange(income)){
+                continue;
+            }
+
+            // Зарплата менеджеров.
+            double managerSalary = income * managerPercent;
+            double pureIncome = income - managerSalary -
+                    calculateCharges();
+
+            double taxAmount = mainTaxPercent * pureIncome;
+            double pureIncomeAfterTax = pureIncome - taxAmount;
+
+            boolean canMakeInvestments = pureIncomeAfterTax >=
+                    minInvestmentsAmount;
+
+            System.out.println("Зарплата менеджера: $" + managerSalary);
+            System.out.println("Общая сумма налогов: $" + (taxAmount > 0 ? taxAmount : 0));
+
+            System.out.println("Компания может инвестировать: " +
+                    (canMakeInvestments ?  "Да" : "Нет"));
+            if (pureIncome < 0){
+                System.out.println("Бюджет в минусе! Нужно срочно зарабатывать!");
             }
 
         }
+
+
+    }
+
+    private static boolean checkIncomeRange(int income){
+        if (income < minIncome) {
+            System.out.println("Доход меньше нижней грани.");
+            return false;
+        }
+
+        if (income > maxIncome) {
+            System.out.println("Доход выше верхней грани.");
+            return false;
+        }
+
+        return true;
+    }
+
+    private static int calculateCharges(){
+        return officeRentCharge +
+                telephonyCharge +
+                internetAccessCharge +
+                assistantSalary +
+                financeManagerSalary;
     }
 }
