cf6d69fccaf359fa1b3e8852125a569ec563f29f
Yuriy Perezhogin <wolfviking@yandex.ru>
2024-06-07 09:32:17 +0500

    update internetShop/basket add internetShop/Main

--- practis-java/src/internetShop/Basket.java
+++ practis-java/src/internetShop/Basket.java
@@ -2,47 +2,53 @@ package internetShop;
 
 public class Basket {
 
-    private static String items = "";
-    private static int totalPrice;
+    private  String items = "";
+    private int totalPrice;
+    private int limit;
 
-
-    public static void main(String[] args) {
-        add("Колбаса", 76);
-        add("Молоко", 55);
-        add("Масло", 120);
-        add("Масло", 110);
-        int totalPrice = getTotalPrice();
-        print("Содержимое корзины:");
-        System.out.println("Общая стоимость товаров: " + totalPrice);
-        clear();
-        System.out.println();
-        totalPrice = getTotalPrice();
-        print("Содержимое корзины:");
-        System.out.println("Общая стоимость товаров: " + totalPrice);
+    public Basket() {
+        items = "Список товаров:\n";
+        this.limit = 100000;
     }
 
-    public static void add(String name, int price) {
+    public Basket(int limit) {
+        this();
+        this.limit = limit;
+    }
+
+    public Basket(String items, int totalPrice) {
+        this();
+        this.items = this.items + items;
+        this.totalPrice = totalPrice;
+    }
+
+    public void add(String name, int price) {
         if (contains(name)){
             return;
         }
-        items = items + "\n" + name + " - " + price;
+
+        if(totalPrice + price >= limit){
+            return;
+        }
+
+        items = items  + name + " - " + price + "\n";
         totalPrice += price;
     }
 
-    public static void clear() {
+    public void clear() {
         items = "";
         totalPrice = 0;
     }
 
-    public static boolean contains(String name){
+    public boolean contains(String name){
         return items.contains(name);
     }
 
-    public static int getTotalPrice() {
+    public int getTotalPrice() {
         return totalPrice;
     }
 
-    public static void print(String title){
+    public void print(String title){
         System.out.println(title);
         if (items.isEmpty()) {
             System.out.println("Корзина пуста.");
---
+++ practis-java/src/internetShop/Main.java
@@ -0,0 +1,21 @@
+package internetShop;
+
+public class Main {
+    public static void main(String[] args) {
+        Basket vasyaBasket = new Basket();
+
+        vasyaBasket.add("Молоко", 80);
+        vasyaBasket.add("Хлеб", 40);
+
+        Basket petyaBasket = new Basket(500);
+        petyaBasket.add("Лопата", 280);
+        petyaBasket.add("Бочка", 2900);
+
+        Basket mashaBasket = new Basket("Стол", 5000);
+
+        vasyaBasket.print("Корзина Васи:");
+        petyaBasket.print("Корзина пети:");
+        mashaBasket.print("Корзина Маши:");
+
+    }
+}
