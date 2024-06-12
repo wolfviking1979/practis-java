e58988d5590815cc9b9e1c271c8cd4938e969d0b
Yuriy Perezhogin <wolfviking@yandex.ru>
2024-06-12 19:16:08 +0500

    add JavaRush/Base/Cicle/WorkForPenny

---
+++ practis-java/src/JavaRush/Base/Cicle/WorkForPenny.java
@@ -0,0 +1,12 @@
+package JavaRush.Base.Cicle;
+
+public class WorkForPenny {
+    public static void main(String[] args) {
+        int index = 0;
+
+        while (index <= 100) {
+            System.out.println("Я никогда не буду работать за копейки!");
+            index++;
+        }
+    }
+}
