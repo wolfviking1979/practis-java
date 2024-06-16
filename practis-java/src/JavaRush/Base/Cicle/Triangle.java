66c5c2efc874d4f2a5831dc4ff5b59ebde4ea11c
Yuriy Perezhogin <wolfviking@yandex.ru>
2024-06-16 13:12:49 +0500

    update JavaRush/Base/Cicle/Triangle

---
+++ practis-java/src/JavaRush/Base/Cicle/Triangle.java
@@ -0,0 +1,12 @@
+package JavaRush.Base.Cicle;
+
+public class Triangle {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("8");
+            }
+            System.out.println(" ");
+        }
+    }
+}
